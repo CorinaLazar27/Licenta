61180bceb75616bccdc4b7a7e9133a31b5308631
CorinaLazar27 <corina_lazar27@yahoo.com>
2022-06-03 16:32:37 +0300

    update new

--- src/Components/RegisterEvent.js
+++ src/Components/RegisterEvent.js
@@ -21,7 +21,7 @@ function RegisterEventPage() {
   const [loading, setLoading] = useState(false);
   const [dataForm, setDataForm] = useState(new Date().toLocaleDateString());
   const history = useHistory();
-  const email = window.localStorage.getItem("email");
+  const emailLocalStorage = window.localStorage.getItem("email");
 
   function FormOptions(values) {
     console.log(values.event);
@@ -33,7 +33,7 @@ function RegisterEventPage() {
       method: "POST",
       url: "https://server-licenta.azurewebsites.net/postform",
       data: {
-        email: email,
+        email: emailLocalStorage,
         event: values.event,
         date: dataForm,
         nrguests: values.nrguests,
@@ -140,8 +140,8 @@ function RegisterEventPage() {
       .required("Introdu bugetul aproximativ alocat evenimentului"),
     judet: Yup.string().required("Trebuie aleasă o opțiune!"),
     dataEvenimentMobile: Yup.string().matches(
-      /^[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/,
-      "Trebuie aleasă o data de tipul zz.mm.zzzz!"
+      /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
+      "Trebuie aleasă o data de tipul zz mm zzzz!"
     ),
   });
 
@@ -261,13 +261,13 @@ function RegisterEventPage() {
       >
         <Formik
           initialValues={{
-            event: "Nuntă",
+            event: "",
             dataEveniment: new Date().toLocaleDateString(),
-            nrguests: "33",
+            nrguests: "",
             dataEvenimentMobile: new Date().toLocaleDateString(),
             location: "",
-            judet: "Alba",
-            budget: "33",
+            judet: "",
+            budget: "",
             liveBand: "",
             artisticMoment: "",
             photographer: "",
